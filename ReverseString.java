c3078a011a99363f3e34697c47115cfffc8ff745
shiva18christ <ratnashiva.sing@mscsa.christuniversity.in>
2025-05-22 14:39:23 +0530

    Reverse a String

---
+++ ReverseString.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class ReverseString {//Reverse a String considering
+    // it is an array of characters
+
+    public void reverse(char[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+
+    }
+
+    public void swap(char[] arr, int start, int end) {
+        char temp = arr[end];
+        arr[end] = arr[start];
+        arr[start] = temp;
+    }
+
+    public static void main(String[] args) {
+        ReverseString string = new ReverseString();
+        char[] arr = {'A', 'P', 'P', 'L', 'E'};
+        string.reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
